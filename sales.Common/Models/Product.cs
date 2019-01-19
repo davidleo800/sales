96210bfc4575d0d6a6af0dc86049afa1520c4489
David <daviud@live.com>
2019-01-19 14:55:49 -0500

    mvc

---
+++ sales.Common/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace sales.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    //Using dentro del namespace 
+    public class Product
+    {
+        [Key]//Autoincremental sql DataAnnotations
+        public int ProductId { get; set; }
+        [Required] //Campo Obligatorio
+        public String Description{ get; set; }
+        public Decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime PublishOn{ get; set; }
+    }
+}
